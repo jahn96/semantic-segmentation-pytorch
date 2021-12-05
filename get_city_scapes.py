6f34d092d65e12997c96fd88f0bedd8a52d6067b
jahn <aju960219@gmail.com>
2021-12-04 17:24:29 -0800

    Add a script to create odgt files for cityscapes

---
+++ get_city_scapes.py
@@ -0,0 +1,43 @@
+import os, json
+from pathlib import Path
+# from PIL import Image
+
+def get_wh(path):
+  # img = Image.open(path)
+
+  # # get width and height
+  # width = img.width
+  # height = img.height
+
+  # return width, height
+  return 2048, 1024
+
+
+def write_data_odgt(image_path, segm_path, file_path, DATA_SIZE=10):
+  with open(file_path, 'w') as fw:
+    for subdir, dirs, files in os.walk(image_path):
+      for dir in dirs:
+        img_path_list = sorted(list(Path(image_path + '/' + dir).glob('*.png')))[:DATA_SIZE]
+        annot_path_list = sorted(list(Path(segm_path + '/' + dir).glob('*labelIds.png')))[:DATA_SIZE]
+
+        assert len(img_path_list) == len(annot_path_list)
+
+        for i in range(len(img_path_list)):
+          elm = {}
+          img_path = str(img_path_list[i])
+          annot_path = str(annot_path_list[i])
+
+          elm['fpath_img'] = img_path
+          elm['fpath_segm'] = annot_path
+
+          width, height = get_wh(img_path)
+          elm['width'] = width
+          elm['height'] = height
+          fw.write(json.dumps(elm) + '\n')
+  print(f'Have succesfully wrote data to {file_path}')
+
+write_data_odgt('./city_scapes/leftImg8bit_trainvaltest/leftImg8bit/train', './city_scapes/gtFine_trainvaltest/gtFine/train', './data/city_training.odgt')
+write_data_odgt('./city_scapes/leftImg8bit_trainvaltest/leftImg8bit/val', './city_scapes/gtFine_trainvaltest/gtFine/val', './data/city_validation.odgt')
+write_data_odgt('./city_scapes/leftImg8bit_trainvaltest/leftImg8bit/test', './city_scapes/gtFine_trainvaltest/gtFine/test', './data/city_testing.odgt')
+
+
